6d4c176d45d70b72a57511159af14f3d4d3944ae
rust-master <muhammadzaryabrafique@gmail.com>
2023-11-14 16:26:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const TG = require("telegram-bot-api");
+require("dotenv").config();
+
+const api = new TG({
+  token: process.env.BOT_TOKEN,
+});
+
+const messageProvider = new TG.GetUpdateMessageProvider();
+
+function main() {
+  // Set message provider and start API
+  api.setMessageProvider(messageProvider);
+  api
+    .start()
+    .then(() => {
+      console.log("API is started");
+    })
+    .catch(console.err);
+
+  api.sendMessage({
+    chat_id: "@top_coins_price_alerts",
+    text: "Welcome to the *" + "Telegram Bot" + "*",
+    parse_mode: "Markdown",
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Visit us!",
+            url: "https://github.com/mast/telegram-bot-api",
+          },
+        ],
+      ],
+    },
+  });
+}
+
+main()
