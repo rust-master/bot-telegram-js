8a55ec425170559b08f429dec0a4eca88f7000f7
rust-master <muhammadzaryabrafique@gmail.com>
2023-11-15 11:28:20 +0500

    Prepare a signing client

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const TG = require("telegram-bot-api");
 require("dotenv").config();
-const { DirectSecp256k1HdWallet } = require("@cosmjs/proto-signing");
-const { StargateClient } =  require("@cosmjs/stargate");
+const {
+  IndexedTx,
+  SigningStargateClient,
+  StargateClient,
+} = require("@cosmjs/stargate");
+const { readFile } = require("fs/promises");
+const {
+  DirectSecp256k1HdWallet,
+  OfflineDirectSigner,
+} = require("@cosmjs/proto-signing");
 
-const rpc = "https://rpc.sentry-01.theta-testnet.polypore.xyz"
+const rpc = "https://rpc.sentry-01.theta-testnet.polypore.xyz";
 
-
+async function getAliceSignerFromMnemonic() {
+  return DirectSecp256k1HdWallet.fromMnemonic(
+    (await readFile("./testnet.alice.mnemonic.key")).toString(),
+    {
+      prefix: "cosmos",
+    }
+  );
+}
 
 const api = new TG({
   token: process.env.BOT_TOKEN,
@@ -41,31 +56,48 @@ function main() {
 }
 
 async function generateKey() {
-    const wallet = await DirectSecp256k1HdWallet.generate(24)
-    process.stdout.write(wallet.mnemonic)
-    const accounts = await wallet.getAccounts()
-    console.log("\nMnemonic with 1st account:", accounts[0].address)
+  const wallet = await DirectSecp256k1HdWallet.generate(24);
+  process.stdout.write(wallet.mnemonic);
+  const accounts = await wallet.getAccounts();
+  console.log("\nMnemonic with 1st account:", accounts[0].address);
 
-    // cosmos1wfjynp59casleh75dclthuueaunw778g4f82my
-    // margin curious divorce slab cruel waste faculty come fit borrow busy solution cake major husband strategy arrive tape increase power cabbage sample bird library
+  // cosmos1wfjynp59casleh75dclthuueaunw778g4f82my
+  // margin curious divorce slab cruel waste faculty come fit borrow busy solution cake major husband strategy arrive tape increase power cabbage sample bird library
 }
-
-
-
 
 async function web3Cosmos() {
-    generateKey()
+  generateKey();
 
-    const client = await StargateClient.connect(rpc)
-    console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight())
+  const client = await StargateClient.connect(rpc);
+  console.log(
+    "With client, chain id:",
+    await client.getChainId(),
+    ", height:",
+    await client.getHeight()
+  );
 
-    console.log(
-        "Balances:",
-        await client.getAllBalances("cosmos1wfjynp59casleh75dclthuueaunw778g4f82my")
-    )
+  console.log(
+    "Balances:",
+    await client.getAllBalances("cosmos1wfjynp59casleh75dclthuueaunw778g4f82my")
+  );
 
+  const aliceSigner = await getAliceSignerFromMnemonic();
+  const alice = (await aliceSigner.getAccounts())[0].address;
+  console.log("Alice's address from signer", alice);
+
+  const signingClient = await SigningStargateClient.connectWithSigner(
+    rpc,
+    aliceSigner
+  );
+
+  console.log(
+    "With signing client, chain id:",
+    await signingClient.getChainId(),
+    ", height:",
+    await signingClient.getHeight()
+  );
 }
 
-web3Cosmos()
+web3Cosmos();
 
 // main();
