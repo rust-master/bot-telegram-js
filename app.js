4250b12c9f8a26c3daae8a44b1fed76884cb1a88
rust-master <muhammadzaryabrafique@gmail.com>
2023-11-15 14:38:00 +0500

    send token to another account and msg to telegram

--- app.js
+++ app.js
@@ -4,37 +4,41 @@ const { SigningStargateClient, StargateClient } = require("@cosmjs/stargate");
 const { readFile } = require("fs/promises");
 const { DirectSecp256k1HdWallet } = require("@cosmjs/proto-signing");
 
-const rpc = "https://rpc.sentry-01.theta-testnet.polypore.xyz";
+const rpc = "https://rpc.sentry-02.theta-testnet.polypore.xyz";
 
 const api = new TG({ token: process.env.BOT_TOKEN });
 const messageProvider = new TG.GetUpdateMessageProvider();
 
-async function getAliceSignerFromMnemonic() {
+async function getAccount1SignerFromMnemonic() {
   return DirectSecp256k1HdWallet.fromMnemonic(
-    (await readFile("./testnet.alice.mnemonic.key")).toString(),
+    (await readFile("./testnet.account1.mnemonic.key")).toString(),
     {
       prefix: "cosmos",
     }
   );
 }
 
+
 async function monitorBalance() {
+  console.log("run")
   const client = await StargateClient.connect(rpc);
   console.log("With client, chain id:", await client.getChainId());
   console.log("Height:", await client.getHeight());
 
-  const aliceSigner = await getAliceSignerFromMnemonic();
-  const alice = (await aliceSigner.getAccounts())[0].address;
-  console.log("Alice's address from signer", alice);
+  const account1Signer = await getAccount1SignerFromMnemonic();
+  const account1 = (await account1Signer.getAccounts())[0].address;
+  console.log("Account1's address from signer", account1);
 
-  let response = await client.getAllBalances(alice);
+  let response = await client.getAllBalances(account1);
 
-  console.log("Alice's Balances:", response);
+  console.log("Account1's Balances:", response);
   console.log("Balance Amount:", response[0].amount);
 
   if (response[0].amount >= 5000000) {
     sendMessageTelegram("Conditon Met");
     console.log("\n\t\t\t*Conditon Met*");
+
+    sendTokens(account1Signer, account1, 4999500);
   } else {
     sendMessageTelegram("Conditon Not Met");
     console.log("\n\t\t\t*Conditon Not Met*");
@@ -68,4 +72,26 @@ async function sendMessageTelegram(msg) {
     .catch(console.err);
 }
 
-setInterval(monitorBalance, 10 * 1000);
+async function sendTokens(signer, from, amount) {
+  const to = "cosmos1pgns8f055aqrsk0f6hwp6x9ffffwtq25r0kmkr";
+  const signingClient = await SigningStargateClient.connectWithSigner(
+    rpc,
+    signer
+  );
+
+  const result = await signingClient.sendTokens(
+    from,
+    to,
+    [{ denom: "uatom", amount: "1000" }],
+    {
+      amount: [{ denom: "uatom", amount: "500" }],
+      gas: "200000",
+    }
+  );
+
+  console.log("Transfer result:", result);
+  sendMessageTelegram(result.transactionHash) 
+
+}
+
+setInterval(monitorBalance, 30 * 1000);
